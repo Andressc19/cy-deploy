c16b3b89f9cfa7ace85486633c6233c41eeddcbd
Andrés Camperos <joseandrescram1005@gmail.com>
2025-08-25 15:45:07 -0500

    fix: change mysql container to postgres

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # cy-deploy
+
+Command to up:
+```bash
+    docker compose up --build -d
+```
+
+Command to down container:
+```bash
+    docker compose down -v ; rm -r postgrest_data
+```
--- init.sql
+++ init.sql
@@ -4,17 +4,13 @@
 --   Version       : 1.0
 -- ============================================================
 
-CREATE DATABASE IF NOT EXISTS cydb;
-
-USE cydb;
-
 CREATE TABLE IF NOT EXISTS usuarios (
-    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     fecha_nacimiento DATE NOT NULL,
     telefono VARCHAR(15) NOT NULL,
     direccion VARCHAR(100) NOT NULL,
-    salario_base DECIMAL(10, 2) NOT NULL
-)
+    salario_base NUMERIC(10, 2) NOT NULL
+);

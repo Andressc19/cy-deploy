c866eabbff98b98fd7dbf8ec5643281ef1de9627
Andressc19 <joseandrescram1005@gmail.com>
2025-09-03 05:02:14 -0500

    fix: maximize number range for amounts in loantype table

--- sql_scripts/init_request.sql
+++ sql_scripts/init_request.sql
@@ -16,8 +16,8 @@ CREATE TABLE IF NOT EXISTS estado_prestamo (
 CREATE TABLE IF NOT EXISTS tipo_prestamo (
     id_tipo_prestamo SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
-    monto_minimo NUMERIC(10,2) NOT NULL,
-    monto_maximo NUMERIC(10,2) NOT NULL,
+    monto_minimo NUMERIC(15,2) NOT NULL,
+    monto_maximo NUMERIC(15,2) NOT NULL,
     tasa_interes NUMERIC(5,2) NOT NULL,
     validacion_automatica BOOLEAN DEFAULT false
 );
@@ -31,8 +31,21 @@ CREATE TABLE IF NOT EXISTS solicitud_prestamo (
     id_estado INT NOT NULL,
     id_tipo_prestamo INT NOT NULL,
     fecha_aprobacion DATE,
-    fecha_creacion DATE NOT NULL,
     
     CONSTRAINT fk_solicitud_estados FOREIGN KEY (id_estado) REFERENCES estado_prestamo(id_estado),
     CONSTRAINT fk_solicitud_tipos_prestamos FOREIGN KEY (id_tipo_prestamo) REFERENCES tipo_prestamo(id_tipo_prestamo)
 );
+
+-- 1. Insertar estados de prestamo
+INSERT INTO estado_prestamo(id_estado, nombre, descripcion) VALUES
+    (1, 'PENDIENTE', 'Pendiente de revisión'),
+    (2, 'APROBADO', 'Prestamo aprobado');
+
+-- 2. Insertar tipos de préstamo
+INSERT INTO tipo_prestamo (nombre, monto_minimo, monto_maximo, tasa_interes, validacion_automatica) VALUES
+('Personal', 2000000, 40000000, 12.5, true),
+('Hipotecario', 200000000, 2000000000, 7.0, false),
+('Automotriz', 20000000, 200000000, 9.5, true),
+('Estudiantil', 4000000, 80000000, 5.0, true),
+('Consolidación deuda', 8000000, 200000000, 11.0, false),
+('Microcrédito', 400000, 20000000, 15.0, true);
